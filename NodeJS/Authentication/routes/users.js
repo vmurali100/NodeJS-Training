7c6335242d86762d030708440608ce2bf161145b
vmurali100 <vmurali100@gmail.com>
2021-09-30 18:39:40 +0530

    Adding Changes

--- NodeJS/Authentication/routes/users.js
+++ NodeJS/Authentication/routes/users.js
@@ -1,11 +1,10 @@
 var express = require("express");
 var router = express.Router();
-var User = require("../models/user");
 var validation = require('./validation')
+var allUsers = require('./usersDetails.json')
 /* GET users listing. */
 router.get("/",validation, async function (req, res, next) {
-  
-  res.send({info:"Some users Information "});
+  res.send({info:"Some users Conidentin  Information ",users:allUsers});
 });
 
 module.exports = router;
