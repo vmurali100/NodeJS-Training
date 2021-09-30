9a05cab341d38f71ce5b52014ad80848fac0411e
vmurali100 <vmurali100@gmail.com>
2021-09-30 17:32:24 +0530

    Code Correction

--- NodeJS/Childprocess/exec_file_demo.js
+++ NodeJS/Childprocess/exec_file_demo.js
@@ -1,14 +1,7 @@
-const { execFile } = require("child_process");
-
-execFile('./somefile.exe',(error,stdout,stderr)=>{
-    if(error){
-        console.log(`Error : ${error.message}`)
-        return
-    }
-    if(stderr){
-        console.log(`Error : ${stderr }`)
-        return 
-    }
-
-    console.log(`Stdout : ${stdout}`)
-})
+const { execFile } = require('child_process');
+const child = execFile('node', ['--version'], (error, stdout, stderr) => {
+  if (error) {
+    throw error;
+  }
+  console.log(stdout);
+});
