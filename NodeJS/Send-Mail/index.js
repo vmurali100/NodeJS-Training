41f83f0b6f89c51d9773c72b5c163a5a67c92ecb
vmurali100 <vmurali100@gmail.com>
2021-10-01 15:58:18 +0530

    Sending Mails

---
+++ NodeJS/Send-Mail/index.js
@@ -0,0 +1,26 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'vmurali100@gmail.com',
+    pass: 'Testing!23'
+  }
+});
+
+var mailOptions = {
+  from: 'vmurali100@gmail.com',
+  to: 'murali.veesambattu@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: `Hi Smartherd, thank you for your nice Node.js tutorials.
+          I will donate 50$ for this course. Please send me payment options.`
+  // html: '<h1>Hi Smartherd</h1><p>Your Messsage</p>'        
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
